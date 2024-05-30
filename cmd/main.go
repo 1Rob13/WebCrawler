bb3f0ed5d8b71606802693286d2ed0210c7aeab5
Rob <funRobin91@gmail.com>
2024-05-30 19:35:55 +0200

    removing first idea of using fetch as go routine and add it to crawl and go increase tyime performaance

--- cmd/main.go
+++ cmd/main.go
@@ -8,61 +8,37 @@ import (
 type Fetcher interface {
 	// Fetch returns the body of URL and
 	// a slice of URLs found on that page.
-	Fetch(url string, c chan FetchResult)
-}
-
-type FetchResult struct {
-	Body string
-	Urls []string
-	Err  error
+	Fetch(url string) (body string, urls []string, err error)
 }
 
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
 func Crawl(url string, depth int, fetcher Fetcher) {
-	// TODO: Fetch URLs in parallel.
-	// 1. for that to work i need to run the function as a go routine,
-	// 2.  when they are in outside goroutines i cant get the response directly
-	// 3. 1 channel with a struct
-
-	c := make(chan FetchResult)
-
-	//the channel needs to be passed to go routine
-
-	// TODO: Don't fetch the same URL twice.
+	// TODO: Fetch URLs in parallel. results need to hit channel
+	// TODO: Don't fetch the same URL twice. -> with context
 	// This implementation doesn't do either:
 	if depth <= 0 {
 		return
 	}
-	go fetcher.Fetch(url, c)
-
-	//add w8 group TODO
-
-	//when w8 group is ended, close channel and read?!
-
-	for v := range c {
-		if v.Err != nil {
-			fmt.Println(v.Err)
-			return
-		}
-		fmt.Printf("found: %s %q\n", v.Urls, v.Body)
-		for _, u := range v.Urls {
-			Crawl(u, depth-1, fetcher)
-		}
-
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-
-	close(c)
-
+	fmt.Printf("found: %s %q\n", body, urls)
+	for _, u := range urls {
+		go Crawl(u, depth-1, fetcher)
+	}
 	return
 }
 
 func main() {
 
-	start := time.Now()
+	now := time.Now()
+
 	Crawl("https://golang.org/", 4, fetcher)
 
-	fmt.Printf("time since start %v", time.Since(start))
+	fmt.Println(time.Since(now))
 }
 
 // fakeFetcher is Fetcher that returns canned results.
@@ -73,12 +49,11 @@ type fakeResult struct {
 	urls []string
 }
 
-func (f fakeFetcher) Fetch(url string, c chan FetchResult) {
-
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
 	if res, ok := f[url]; ok {
-		c <- FetchResult{Body: res.body, Urls: res.urls, Err: nil}
+		return res.body, res.urls, nil
 	}
-	c <- FetchResult{Body: "", Urls: nil, Err: fmt.Errorf("not found: %s", url)}
+	return "", nil, fmt.Errorf("not found: %s", url)
 }
 
 // fetcher is a populated fakeFetcher.
