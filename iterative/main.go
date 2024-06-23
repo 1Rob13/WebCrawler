7c7447f61daf2487a3e8f1d9662c2a3bbc4d80da
Rob <funRobin91@gmail.com>
2024-06-23 19:02:36 +0200

    new version with just a parralel function not a paralle workload

--- iterative/main.go
+++ iterative/main.go
@@ -2,22 +2,89 @@ package main
 
 import (
 	"fmt"
-	"slices"
+	"sync"
 	"time"
 )
 
-type fakeFetcher map[string]*fakeResult
+func FetchParallel(urls []string) []string {
+
+	fmt.Println("got called")
+	fmt.Println(urls)
+
+	ch := make(chan []string)
+
+	var wg sync.WaitGroup
+
+	for _, city := range urls {
+
+		wg.Add(1)
+		go fetch(city, ch, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	resultUrls := []string{}
+
+	for result := range ch {
+
+		fmt.Println(result)
+		resultUrls = append(resultUrls, result...)
+	}
+
+	return resultUrls
+
+}
 
 type fakeResult struct {
 	body string
 	urls []string
 }
 
-func (f fakeFetcher) Fetch(url string) (string, []string, error) {
-	if res, ok := f[url]; ok {
-		return res.body, res.urls, nil
+type fakeFetcher map[string]*fakeResult
+
+func fetch(url string, ch chan []string, wg *sync.WaitGroup) {
+
+	fetcher := fakeFetcher{
+		"https://golang.org/": &fakeResult{
+			"The Go Programming Language",
+			[]string{
+				"https://golang.org/pkg/",
+				"https://golang.org/cmd/", // a miss
+			},
+		},
+		"https://golang.org/pkg/": &fakeResult{
+			"Packages",
+			[]string{
+				"https://golang.org/",     // already traversed
+				"https://golang.org/cmd/", // a miss
+				"https://golang.org/pkg/fmt/",
+				"https://golang.org/pkg/os/",
+			},
+		},
+		"https://golang.org/pkg/fmt/": &fakeResult{
+			"Package fmt",
+			[]string{
+				"https://golang.org/",
+				"https://golang.org/pkg/",
+			},
+		},
+		"https://golang.org/pkg/os/": &fakeResult{
+			"Package os",
+			[]string{
+				"https://golang.org/",
+				"https://golang.org/pkg/",
+			},
+		}}
+
+	if res, ok := fetcher[url]; ok {
+		ch <- res.urls
 	}
-	return "", nil, fmt.Errorf("not found: %s", url)
+
+	defer wg.Done()
+
 }
 
 func main() {
@@ -25,137 +92,51 @@ func main() {
 	now := time.Now()
 
 	var (
-		fetcher = fakeFetcher{
-			"https://golang.org/": &fakeResult{
-				"The Go Programming Language",
-				[]string{
-					"https://golang.org/pkg/",
-					"https://golang.org/cmd/", // a miss
-				},
-			},
-			"https://golang.org/pkg/": &fakeResult{
-				"Packages",
-				[]string{
-					"https://golang.org/",     // already traversed
-					"https://golang.org/cmd/", // a miss
-					"https://golang.org/pkg/fmt/",
-					"https://golang.org/pkg/os/",
-				},
-			},
-			"https://golang.org/pkg/fmt/": &fakeResult{
-				"Package fmt",
-				[]string{
-					"https://golang.org/",
-					"https://golang.org/pkg/",
-				},
-			},
-			"https://golang.org/pkg/os/": &fakeResult{
-				"Package os",
-				[]string{
-					"https://golang.org/",
-					"https://golang.org/pkg/",
-				},
-			}}
-
-		firstUrl  = "https://golang.org/"
-		urls      = []string{}
-		urlsCache = []string{}
+		firstUrl = "https://golang.org/"
+		urls     = []string{}
+		//visited  = []string{}
 	)
 
 	urls = append(urls, firstUrl)
 
 	for j := 0; j < 3; j++ {
-		//for {
-		fmt.Println("-------------------------")
 
-		fmt.Printf("iteration: ( %v )\n", j)
-		fmt.Printf("urls: ( %v )\n", urls)
-		fmt.Printf("cached urls: ( %v )\n\n\n\n", urlsCache)
+		//	fmt.Println()
 
-		if len(urls) == 0 {
-			break
-		}
+		// newUrls := []string{}
 
-		//parallisable from here------------------------------------------------
-		//
-		//
-		// -- cant be done inside the loop because that would be done double for each url
-		//  it will be reset during next iteration
-		// chURLS := make(chan []string)
+		// for _, url := range urls {
 
-		//is this for loop taking more than normal? it should be at init fixed how many urls
-		for _, url := range urls {
+		// 	if slices.Contains(visited, url) {
 
-			urlsCache = append(urlsCache, url) // this should be doable after because the barrier of the semaphore happens later anyway
+		// 		continue
+		// 	}
 
-			fmt.Printf("url selected in urls: ( %s)\n", url)
-
-			fmt.Printf("attempt fetch of %s\n", url)
-
-			body, routinesUrls, err := fetcher.Fetch(url)
-			if err != nil {
-				fmt.Printf("routine fetched body: %v:", err)
-			}
-			fmt.Printf("routine fetched body: (%s), found URLs: (%v)\n\n\n\n", body, routinesUrls)
-
-			//if new urls was already visited do not add it to urls
-			for _, url := range routinesUrls {
-
-				if slices.Contains(urlsCache, url) {
-					continue
-				}
-				urls = append(urls, url)
-			}
-
-			//send filtered urls to channel
-			//chURLS <- urls
-
-			//done channels do not work because there are 2 routines that will both end the waiting
-			// done <- 0
-
-			//
-			//
-			//
-			//parallisable to here------------------------------------------------
-
-		}
-
-		// for value := range chURLS {
-
-		// 	fmt.Println(value) // Process the received values
-
+		// 	newUrls = append(newUrls, url)
 		// }
 
-		//cant pull this in for now, will end up new fetched, it kinda works semaphore like because it will wait for other processes to be done
-		//this also saves extra lookups this way
-		newUrls := []string{}
-		//assert that urls is only new urls
-		for _, url := range urls {
+		// urls = newUrls
 
-			if slices.Contains(urlsCache, url) {
-				continue
-			}
+		urlsNew := FetchParallel(urls)
 
-			newUrls = append(newUrls, url)
+		urls = append(urls, urlsNew...)
 
-		}
-
-		urls = newUrls
+		//visited = append(visited, urls...)
 
 	}
 
-	fmt.Printf("\nfetched URLS: %v\n", urls)
-	fmt.Printf("\ncached URLS: %v\n", urlsCache)
+	// fmt.Printf("\nfetched URLS: %v\n", urls)
+	// fmt.Printf("\ncached URLS: %v\n", urlsCache)
 
-	if len(urlsCache) != 5 {
-		fmt.Println("ERROR--------------urlsCache not 5----------------ERROR------------------------------ERROR------------------------------ERROR------------------------------")
+	// if len(urlsCache) != 5 {
+	// 	fmt.Println("ERROR--------------urlsCache not 5----------------ERROR------------------------------ERROR------------------------------ERROR------------------------------")
 
-	}
+	// }
 
-	if len(urls) != 0 {
-		fmt.Println("ERROR--------------urls not 0----------------ERROR------------------------------ERROR------------------------------ERROR------------------------------")
+	// if len(urls) != 0 {
+	// 	fmt.Println("ERROR--------------urls not 0----------------ERROR------------------------------ERROR------------------------------ERROR------------------------------")
 
-	}
+	// }
 
 	fmt.Println(time.Since(now))
 
--- iterative/readme.txt
+++ iterative/readme.txt
@@ -15,3 +15,20 @@ todo make it work without global variables
 
 todo make it work without global variables, while not using parral 
 todo add parallel
+
+functions that are parallelized never contain any return values.
+they also never use values that are not in their channel scope, they do not use globals
+
+i need sth like:
+
+for _,url := range urls {
+
+    go CrawlWithoutDuplicate(channels..,waitgroup)
+
+}
+
+todo test this concept and ensure 2 functions running at the same time
+
+fmt.Println() is slow as fuck
+
+fmt.Printf is somehow slipery when used in parallel
